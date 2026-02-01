bd8b771b872251a5f050ed7e6b45df3101b8fa30
RaviGahire <ravigahire3@gmail.com>
2026-02-01 23:12:49 +0530

    verify jwt token

---
+++ back-end/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.JWT_SECRET; //const secret key
+
+exports.verifyJwtToken = (req, res, next) => {
+  const authHeader = req.headers.authorization; // get bearer token from client
+  // console.log(authHeader)
+
+  if (!authHeader) {
+    //check header
+    res
+      .status(401)
+      .json({ success: false, message: "Authorization header is missing" });
+  }
+
+  const token = authHeader.split(" ")[1]; // split the token string and extract token
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Token is missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey); // verify
+    req.user = decoded;
+
+    //  console.log('Token verifyed successfully')
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
