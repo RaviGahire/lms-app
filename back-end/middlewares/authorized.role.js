f4c631f812bbae6cbbb257d2e6d4f5b7431ebbfb
RaviGahire <ravigahire3@gmail.com>
2026-02-01 23:12:09 +0530

    RBAC middleware

---
+++ back-end/middlewares/authorized.role.js
@@ -0,0 +1,28 @@
+const authorizedRoles = (...roles) => {
+
+  return (req, res, next) => {
+
+ 
+    if (!req.user || !req.user.role) { // safety check
+      return res.status(401).json({
+        success: false,
+        message: "User not authenticated"
+      });
+    }
+
+    const userRole = req.user.role;
+
+    
+    if (!roles.includes(userRole)) { // role check
+      return res.status(403).json({
+        success: false,
+        message: "Access denied"
+      });
+    }
+
+    next();
+  };
+
+};
+
+module.exports = authorizedRoles;
