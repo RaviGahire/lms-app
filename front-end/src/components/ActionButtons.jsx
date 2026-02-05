f670793329ea4923efd2b2f383b72492459f40bc
RaviGahire <ravigahire3@gmail.com>
2026-02-05 13:57:52 +0530

    Changed some update logic but still there is bugs

--- front-end/src/components/ActionButtons.jsx
+++ front-end/src/components/ActionButtons.jsx
@@ -1,26 +1,31 @@
 import axios from "axios"
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 //
-export const UpdateButton = ({ apiUrl, userId, updatedData }) => {
+export const UpdateButton = ({ componetRoute, apiUrl, userId, updatedData, clearData }) => {
+
+    // console.log('from update btn',updatedData )
+
 
     //update handler function 
-    const handleUpdate = async () => {
+    const handleUpdate = async (e) => {
+        e.preventDefault()
         try {
-            const res = await axios.put(`${apiUrl}/${userId}`, updatedData)
-            // if 
-            if (res.data?.message) {
-                alert(res.data.message);
-            } else {
-                alert(res.data.message);
-            }
+            const res = await axios.put(
+                `${apiUrl}/${userId}`,
+                updatedData
+            )
+            alert(res.data?.message || "Updated successfully")
+            Navigate(componetRoute)
+
         } catch (error) {
             console.error(error);
             alert("Update failed");
         }
     }
     return (
-        <button onClick={handleUpdate} class="px-4 py-2 rounded-md cursor-pointer bg-blue-600 text-white text-sm font-medium hover:bg-blue-700 transition">
+        <button type="submit" onClick={(e) => handleUpdate(e)} class="px-4 py-2 rounded-md cursor-pointer bg-blue-600 text-white text-sm font-medium hover:bg-blue-700 transition">
             Update
         </button>
     )
