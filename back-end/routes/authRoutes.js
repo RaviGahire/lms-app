610309ae2971a2f669150ba72bededc36315f157
RaviGahire <ravigahire3@gmail.com>
2026-03-12 14:08:04 +0530

    add password crud and account crud routes

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // auth-controllers 
-const { userRegister ,generateEmailVerificationOtp } = require('../controllers/auth.controller');
+const { userRegister ,generateEmailVerificationOtp, userLogin, getCurrentUser, getAllUsers, deleteUser, userAccountDeleteRequest, changeCurrentUserPassword, forgotPassword } = require('../controllers/auth.controller');
 const { verifyUserOtp } = require('../middlewares/otp.middleware');
+const { verifyJwtToken } = require('../middlewares/auth.middleware');
 
 //Email and OTP verification routes
 router.post('/send-email-otp', generateEmailVerificationOtp);
@@ -16,6 +17,25 @@ router.post('/verify-otp', verifyUserOtp,  (_, res) => {
 router.post('/register',userRegister);
 
 //login user 
+router.post('/login' , userLogin)
+
+//current user 
+router.get('/current-user',verifyJwtToken,getCurrentUser)
+//all users
+router.get('/all/users',getAllUsers)
+
+//CRUD OPS ROUTES ON USER 
+
+//Admin can deleted user using id 
+router.delete('/delete/u/:id',deleteUser)
+
+//user request to delete account 
+router.delete('/delete/request',verifyJwtToken, userAccountDeleteRequest)
+
+//change current user password 
+router.patch('/change/p' , verifyJwtToken , changeCurrentUserPassword)
+
+router.patch("/forgot-password",forgotPassword)
 
 
 
