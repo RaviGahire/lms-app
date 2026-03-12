d4d479f5a847d461458f2bdddb3fc945d9338901
RaviGahire <ravigahire3@gmail.com>
2026-03-12 14:06:45 +0530

    changed secrets and minor improvment

--- back-end/middlewares/auth.middleware.js
+++ back-end/middlewares/auth.middleware.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
-const secretKey = process.env.JWT_SECRET; 
+const User = require("../model/user.model")
 
-exports.verifyJwtToken = (req, res, next) => {
-  const authHeader = req.headers.authorization; // get bearer token from client
+exports.verifyJwtToken = async (req, res, next) => {
+
+  const authHeader = req.headers.authorization // get bearer token from client
+
   // console.log(authHeader)
 
   if (!authHeader) {
     //check header
-    res.status(401).json({ success: false, message: "Authorization header is missing" });
+    return res.status(401).json({ success: false, message: "Authorization header is missing" });
   }
 
   const token = authHeader.split(" ")[1]; // split the token string and extract token
@@ -19,11 +21,19 @@ exports.verifyJwtToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, secretKey); // verify
-    req.user = decoded;
+    const decodedToken = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
 
-    // console.log('Token verifyed successfully')
+    const user = await User.findById(decodedToken?._id).select("-password");
 
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    req.user = user; // adding new property to req
+   
     next();
   } catch (error) {
     return res.status(401).json({
