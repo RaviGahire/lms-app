d48560c6c82d34c5f62308d0526b29a996b7991c
RaviGahire <ravigahire3@gmail.com>
2026-04-03 21:24:08 +0530

    Add re-named components and added nessecery home page components

---
+++ front-end/src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import { PlatformOverview } from './PlatformOverview';
+import { HomeHero } from './HomeHero';
+import { SuccessAndFeatures } from './SuccessAndFeatures'
+import { MasterTrack } from './WhatIsMasterTrack';
+import { Footer } from '../../components/Footer'
+
+export const Home = () => {
+  return (
+    <>
+      {/* Hero Section */}
+      <HomeHero />
+
+      {/* What is master track */}
+      <MasterTrack />
+
+      {/* Success And Features Section */}
+      <SuccessAndFeatures />
+
+      {/* Platform Overview Section */}
+      <PlatformOverview />
+
+      {/* Footer*/}
+      <Footer />
+    </>
+  );
+};
+
+
