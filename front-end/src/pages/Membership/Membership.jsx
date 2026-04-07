20b32db38ed1c749a826a0dcd480950d0475d8c1
RaviGahire <ravigahire3@gmail.com>
2026-04-07 20:06:32 +0530

    feat: add Membership and Pricing components with layout and features

---
+++ front-end/src/pages/Membership/Membership.jsx
@@ -0,0 +1,15 @@
+import { Pricing } from "./Pricing"
+import { Banner } from "../Partials/Banner"
+import { FAQSection } from "../Partials/FAQSection"
+
+
+export const Membership = () => {
+    return (
+        <>
+            <Pricing />
+            <Banner />
+            <FAQSection />
+
+        </>
+    )
+}
