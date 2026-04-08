7937db20169199fe10efc430735c18b7035fe184
RaviGahire <ravigahire3@gmail.com>
2026-04-08 17:18:30 +0530

    feat: implement course fetching and filtering in ActiveCurriculum component

--- front-end/src/components/Students/ActiveCurriculum.jsx
+++ front-end/src/components/Students/ActiveCurriculum.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+import { getAllCourses } from "../../utils/GetCouresDetails"
+
+export const ActiveCurriculum = ({ enrolled_courses }) => {
+    const [ActiveCurriculum, setActiveCurriculum] = useState([])
+    const courses = enrolled_courses?.course || [];
+    // console.log(courses)
+    // console.log(ActiveCurriculum)
+
+    const filterCourses = ActiveCurriculum.filter((data) => courses.includes(data?._id))
+
+    console.log(filterCourses)
+
+    useEffect(() => {
+        async function AllCourses() {
+            const data = await getAllCourses()
+            setActiveCurriculum(data)
+
+        }
+        AllCourses()
+    }, [])
 
 
-export const ActiveCurriculum = () => {
     return (
         <div>
             <h3 className="text-xl font-semibold mb-6 text-white flex items-center gap-2">
@@ -8,19 +28,13 @@ export const ActiveCurriculum = () => {
                 Active Curriculum
             </h3>
             <div className="space-y-4">
-                {[
-                    { name: "HTML5", progress: 75, color: "bg-purple-500" },
-                    { name: "CSS", progress: 90, color: "bg-blue-500" },
-                    { name: "JavaScript", progress: 85, color: "bg-amber-500" },
-                    { name: "Tailwind", progress: 60, color: "bg-emerald-500" },
-                    { name: "React.js", progress: 85, color: "bg-amber-500" },
-                ].map((course, idx) => (
-                    <div key={idx} className="bg-white/5 border border-white/5 rounded-xl p-5 hover:bg-white/10 transition-colors group">
+                {filterCourses.map((course, idx) => (
+                    <div key={course?._id} className="bg-white/5 border border-white/5 rounded-xl p-5 hover:bg-white/10 transition-colors group">
                         <div className="flex justify-between items-end mb-4">
                             {/* Course Info */}
                             <div className="space-y-1">
                                 <h4 className="font-medium text-gray-100 group-hover:text-white transition-colors">
-                                    {course.name}
+                                    {course.title}
                                 </h4>
                                 <p className="text-xs font-mono text-gray-300">
                                     {course.progress}% Complete
