c050fe89dadfe070909f24a458d7ee810495a901
RaviGahire <ravigahire3@gmail.com>
2026-04-16 19:00:45 +0530

    feat: trying to add data-base conection retry

--- back-end/config/connectDB.js
+++ back-end/config/connectDB.js
@@ -4,18 +4,45 @@ const DATABASE_URL = process.env.DATABASE_URL;
 //database string
 const connectionDB = async () => {
     try {
-
         await mongoose.connect(DATABASE_URL);
-        console.log('Database connected successfully',mongoose.connection.readyState);
-
+        console.log('Database connected successfully', mongoose.connection.readyState);
     } catch (error) {
-        console.log('Database connection failed:', mongoose.connection.readyState );
+        console.log('Database connection failed:', mongoose.connection.readyState);
         console.log(error.message);
-
     }
-
 }
-//Call the function for connection 
+
+// (async () => {
+
+//     try {
+
+//         let status = await connectionDB()
+
+//         if (!status) {
+//             console.log("Retrying once...")
+
+//             await new Promise((res) => setTimeout(res, 4000))
+
+//             status = await connectionDB()
+//         }
+
+//         if (status) {
+//             console.log("Retry successfull database connected..")
+//         } else {
+//             console.log("Retry failed..")
+//         }
+
+//     } catch (error) {
+//         console.log('Database connection failed:', mongoose.connection.readyState);
+//         console.log(error.message);
+//     }
+
+
+// }
+
+// )();
+
 connectionDB()
+
 //export a connection
 module.exports = connectionDB
