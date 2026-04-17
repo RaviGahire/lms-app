8f869b3fdcdd6553773f39449737f3c53ba50c94
RaviGahire <ravigahire3@gmail.com>
2026-04-17 15:16:34 +0530

    fix: simplify user validation logic in ProtectedRoute component

--- front-end/src/components/ProtectedRoute.jsx
+++ front-end/src/components/ProtectedRoute.jsx
@@ -2,10 +2,9 @@ import { Navigate } from "react-router-dom"
 
 
 export const ProtectedRoute = ({ loading, user, allowedUserRoles, children }) => {
+// console.log(user)
 
-    // console.log(user)
-
-    if (loading || (user === null && !user)) {
+    if (loading) {
         return (
             <div className="flex h-screen bg-cyan-800 items-center justify-center">
                 <p className="text-cyan-100 font-medium animate-pulse">
@@ -15,16 +14,16 @@ export const ProtectedRoute = ({ loading, user, allowedUserRoles, children }) =>
         );
     }
 
-    if (user?.role === 'user' || user?.role === '') {
-        return <Navigate to="/verify-profile" />;
-    }
-
-
     if (!user) {
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/login"  />;
     }
 
-    const userRole = user.role?.toLowerCase()
+    // if (!user?.isVerified) {
+    //     return <Navigate to="/verify-profile"  />;
+    // }
+
+
+    const userRole = user.role?.toLowerCase();
 
     if (allowedUserRoles && !allowedUserRoles.includes(userRole)) {
         return <Navigate to="/unauthorized" replace />;
