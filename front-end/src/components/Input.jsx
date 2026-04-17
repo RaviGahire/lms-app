e2ca9213c24b641501f7f85429e2f116097a7853
RaviGahire <ravigahire3@gmail.com>
2026-04-17 15:12:38 +0530

    fix: standardize error handling and improve input component styling

--- front-end/src/components/Input.jsx
+++ front-end/src/components/Input.jsx
@@ -1,59 +1,69 @@
 
-export const Input = ({ label, id, placeholder, name, type, value, onChange, className, isEditable, Error }) => {
-    //handle file input
-    const inputValue = type === 'file' ? undefined : value;
+//form input
+export const Input = ({ label, id, placeholder, name, type, value, onChange, className, isEditable, error }) => {
 
-    return (
-        <div className={`flex flex-col gap-2 relative ${className}`} >
-            {/* label */}
-            <label htmlFor={id} className="text-sm font-semibold text-slate-700">{label}</label>
-            {/* Input  */}
-            <input type={type} id={id} name={name} placeholder={placeholder} disabled={isEditable}
-                value={inputValue}
-                onChange={onChange}
-                className='w-full px-4 py-1.5 disabled rounded-lg border border-slate-300 focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all' />
-            {/* Error Message */}
-            <p className="absolute top-18 text-xs text-red-500 font-medium animate-in fade-in slide-in-from-top-1">{Error}</p>
-        </div>
-    )
+  // console.log(error)
+  // handle file input
+  const inputValue = type === 'file' ? undefined : value;
+
+  return (
+    <div className={`flex flex-col gap-2 relative ${className}`} >
+      {/* label */}
+      <label htmlFor={id} className="text-sm font-semibold text-slate-700">{label}</label>
+      {/* Input  */}
+      <input type={type} id={id} name={name} placeholder={placeholder} disabled={isEditable}
+        value={inputValue}
+        onChange={onChange}
+        className='w-full shadow-sm text-[12px] tracking-wide md:text-[16px] px-2 md:px-3 py-1 md:py-1.5 disabled rounded-md border border-slate-300 focus:ring-1 focus:ring-blue-500/80 focus:outline-none transition-all' />
+      {/* Error Message */}
+      <p className="absolute top-18 text-[8px] md:text-xs text-red-500 font-medium">{error}</p>
+    </div>
+  )
 }
 
 // Text Area
-export function DescriptionInput({ label, id, placeholder, name, value, onChange, className, isEditable, Error, rows, maxLength}) {
+export function DescriptionInput({ label, id, placeholder, name, value, onChange, className, isEditable, rows, maxLength, error }) {
 
-    return (
-        <>
-            <label htmlFor={id} className="text-sm font-semibold text-slate-700">{label}</label>
-            <textarea
-                className="w-full px-4 py-1.5 disabled rounded-lg border border-slate-300 focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all"
-                placeholder={placeholder}
-                value={value}
-                name={name}
-                id={id}
-                rows={rows}
-                onChange={onChange}
-                maxLength={maxLength}
-            />
+  return (
+    <div className={`flex flex-col gap-2 relative ${className}`} >
+      <label htmlFor={id} className="text-sm font-semibold text-slate-700">{label}</label>
+      <textarea
+        className="w-full shadow-sm text-[12px] tracking-wide md:text-[16px] px-2 md:px-3 py-1 md:py-1.5 disabled rounded-md border border-slate-300 focus:ring-1 focus:ring-blue-500/80 focus:outline-none transition-all"
+        placeholder={placeholder}
+        value={value}
+        name={name}
+        id={id}
+        rows={rows}
+        onChange={onChange}
+        maxLength={maxLength}
 
-        </>
-    );
+      />
+      <p className="absolute top-18 text-xs text-red-500 font-medium">{error}</p>
+
+    </div>
+  );
 }
 
 //select input
-export const SelectInput = ({ 
-  label, 
-  name, 
-  id, 
-  value, 
-  onChange, 
-  options = [], 
-  placeholder = "Select a course" 
+export const SelectInput = ({
+  style,
+  label,
+  name,
+  id,
+  value = "",
+  onChange,
+  options = [],
+  placeholder = "Select option",
+  error
 }) => {
+
+  console.log(error)
+
   return (
-    <div className="flex flex-col gap-2 w-full max-w-xs">
-      
+    <div className="flex flex-col gap-2 w-full ">
+
       {label && (
-        <label htmlFor={id} className="text-sm font-semibold text-gray-700">
+        <label htmlFor={id} className="text-sm font-medium text-gray-700">
           {label}
         </label>
       )}
@@ -64,24 +74,34 @@ export const SelectInput = ({
           id={id}
           value={value}
           onChange={onChange}
-          className="block w-full px-4 py-2.5 text-gray-700 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none cursor-pointer transition duration-200"
+          className={`${style} block w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none cursor-pointer transition`}
         >
-           <option value="" disabled>
+          {/* Placeholder */}
+          <option className={`${style} `} value="" disabled>
             {placeholder}
           </option>
 
-                  {options.map((course, index) => (
-            <option key={course.id || index} value={course.title}>
-              {course.title}
+          {/* Options */}
+          {options.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
             </option>
           ))}
         </select>
-        
+
+        {/* Dropdown Icon */}
         <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none text-gray-500">
-          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <svg
+            className="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
           </svg>
         </div>
+        {/* error */}
+        <p className="absolute top-11.5 text-xs text-red-500 font-medium animate-in fade-in slide-in-from-top-1">{error}</p>
       </div>
     </div>
   );
