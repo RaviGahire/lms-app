096507bd41372be1d539cb988024fcc711acf9a9
RaviGahire <ravigahire3@gmail.com>
2026-04-18 13:13:34 +0530

    refactor: restructure  routes using REST principles

--- back-end/index.js
+++ back-end/index.js
@@ -3,30 +3,40 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 
-
-// middlewares
+// ─────────────────────────────────────────
+// All Necessary Middlewares
+// ─────────────────────────────────────────
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(express.static('public/'))
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 
-// db connection
+// ─────────────────────────────────────────
+// Database Connection
+// ─────────────────────────────────────────
 const connectDB = require('./config/connectDB')
 
-//All routes
+// ─────────────────────────────────────────
+// All App Routes
+// ─────────────────────────────────────────
 const authRoutes = require('./routes/auth.routes')
 const studentRoutes = require('./routes/student.routes')
 const courseRoutes = require('./routes/course.routes')
 const instructorRoutes = require('./routes/instructor.routes')
 const adminRoutes = require('./routes/admin.routes')
-//APi endpoints
-app.use('/api/v1/users', authRoutes)
+
+// ─────────────────────────────────────────
+// All API End-points
+// ─────────────────────────────────────────
+app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/students', studentRoutes)
+app.use('/api/v1/instructors', instructorRoutes)
 app.use('/api/v1/courses', courseRoutes)
-app.use('/api/v1/instructor', instructorRoutes)
 app.use('/api/v1/admin', adminRoutes)
 
-//Server Listening
+// ─────────────────────────────────────────
+// Server Listening
+// ─────────────────────────────────────────
 app.listen(process.env.PORT || 5000, process.env.HOST, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`)
 });
