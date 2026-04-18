26b2b283833643dc1e8faf881d502149c3eb0377
RaviGahire <ravigahire3@gmail.com>
2026-04-18 13:30:01 +0530

    improved comments for clear understanding

--- back-end/utils/OtpGenerator.js
+++ back-end/utils/OtpGenerator.js
@@ -1,4 +1,6 @@
-//Otp Generator
+// ─────────────────────────────────────────
+// OTP Generator Function
+// ─────────────────────────────────────────
 const OtpGenerator = async () => {
     const generatedOtp = Math.floor(1000 + Math.random() * 9000);
     return generatedOtp;
