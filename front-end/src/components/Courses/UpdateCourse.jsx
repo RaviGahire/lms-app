c3c4d36aee15810114870da45949b4ff959819c0
RaviGahire <ravigahire3@gmail.com>
2026-04-19 19:56:32 +0530

    refactor: enhance UpdateCourse component with additional form fields and improved structure

--- front-end/src/components/Courses/UpdateCourse.jsx
+++ front-end/src/components/Courses/UpdateCourse.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { DescriptionInput, Input } from '../Input'
+import { DescriptionInput, Input, SelectInput } from '../Input'
+import { UpdateUserDetails } from '../../utils/UpdateUserDetails'
 
 export const UpdateCourse = ({ course }) => {
 
@@ -8,8 +9,12 @@ export const UpdateCourse = ({ course }) => {
         title: course?.title || '',
         description: course?.description || '',
         duration: course?.duration || '',
+        price: course?.price || '',
+        category: course?.category || '',
+        coverImage:course?.coverImage || ''
     })
 
+    console.log(formData)
     const handleChange = (e) => {
         const { name, value } = e.target
         setFormData({ ...formData, [name]: value })
@@ -18,60 +23,102 @@ export const UpdateCourse = ({ course }) => {
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('Updated Data:', formData)
-        
+
     }
 
-     useEffect(() => {
+    useEffect(() => {
         if (course) {
             setFormData({
-                title: course.title || '',
-                description: course.description || '',
-                duration: course.duration || '',
+                title: course?.title || '',
+                description: course?.description || '',
+                duration: course?.duration || '',
+                price: course?.price || '',
+                category: course?.category || '',
+                coverImage: course?.coverImage || '',
             })
         }
-    }, [course]) 
+    }, [course])
 
+    // console.log(course)
     return (
         <div className="w-full max-w-4xl">
-            <div className="bg-white border border-gray-200 rounded-3xl shadow-sm p-8">
+            <div className="p-1.5 md:p-4">
 
                 {/* Heading */}
                 <div className="mb-8">
-                    <h2 className="text-2xl font-bold text-gray-800">Update Course Details</h2>
+                    <h2 className="text-2xl font-bold text-gray-800 underline">Update Course Details</h2>
                     <p className="text-gray-500 mt-1">Fill the details below in existing course</p>
                 </div>
 
-                <form onSubmit={handleSubmit}  className="space-y-6">  
-
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    {/* Title */}
                     <Input
                         label="Title"
+                        type='text'
+                        id='title'
+                        name="title"
                         placeholder="Enter Course Title"
-                        name="title"               
-                        value={formData.title}     
-                        onChange={handleChange}    
+                        value={formData.title}
+                        onChange={handleChange}
                     />
+                    {/*Duration  */}
                     <Input
                         label="Duration"
+                        type='number'
+                        name="duration"
+                        id='duration'
                         placeholder="Enter Course Duration"
-                        name="duration"            
-                        value={formData.duration}  
-                        onChange={handleChange}   
+                        value={formData.duration}
+                        onChange={handleChange}
                     />
+                    {/*Price  */}
+                    <Input
+                        label="Price"
+                        type='number'
+                           name="price"
+                           id='price'
+                        placeholder="Enter Course price"
+                        value={formData.price}
+                        onChange={handleChange}
+                    />
+                    <SelectInput
+                        label='Category'
+                        type='text'
+                        id='category'
+                        name='category'
+                        defaultOption='Select Category'
+                        options={['development', 'design', 'business', 'personal-development', 'it-software', 'data-ai', 'creative', 'language-learning']}
+                        value={formData.category}
+                        onChange={handleChange}
+                    />
+
+                    <Input
+                    label='Cover Image'
+                    type='file'
+                    name='coverImage'
+                    id='coverImage'
+                    value={formData.coverImage}
+                    onChange={handleChange}
+
+                    
+                    />
+                    {/* Description */}
                     <DescriptionInput
                         label="Description"
                         placeholder="Enter Course Description"
-                        name="description"             
-                        value={formData.description}    
-                        onChange={handleChange}         
+                        name="description"
+                        id='description'
+                        value={formData.description}
+                        onChange={handleChange}
                     />
 
                     <div className="flex items-center gap-4 pt-4">
-                        <button
-                            type="submit"
-                            className="px-8 py-3 rounded-xl bg-cyan-600 text-white font-semibold hover:bg-cyan-700 transition shadow-md"
-                        >
-                            Save
-                        </button>
+                     <UpdateUserDetails
+                     label={'save'}
+                     endpoint={`courses/${course?._id}`}
+                     payload={formData}
+                     redirectTo={'/instructor'}
+                     />
                         <button
                             type="button"
                             className="px-8 py-3 rounded-xl border border-gray-200 text-gray-600 hover:bg-gray-50 transition"
