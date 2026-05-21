087fc20fc4f3e76d4e2ed4e3e807bdb81de0b5ab
RaviGahire <ravigahire3@gmail.com>
2026-05-21 17:25:26 +0530

    style: standardize import statements formatting in main.jsx for consistency and removed toastfy import

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import ContextProvider from './contexts/ContextProvider'
-import 'react-toastify/ReactToastify.css';
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ContextProvider from "./contexts/ContextProvider";
+import "./index.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
