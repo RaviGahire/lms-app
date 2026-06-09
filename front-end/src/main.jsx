fe0ce3627123bfeff10bf7638064cd1338f9fea0
RaviGahire <ravigahire3@gmail.com>
2026-06-09 12:02:06 +0530

    trying to fix vercel build error

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,8 +1,8 @@
-import ContextProvider from "./contexts/ContextProvider"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import ContextProvider from "./contexts/ContextProvider";
 import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
